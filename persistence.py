877ee02c999023caa036d65573212275bf2837e5
mohankrishna.mcp <mcpmk1998@gmail.com>
2024-07-02 16:34:10 +0530

    persistency base file

---
+++ persistence.py
@@ -0,0 +1,3 @@
+#implement persistence memory via file management
+#periodically write in-memory data to file
+#when server kv_store starts/restarts - load the file data to memory, so that data is not lost
